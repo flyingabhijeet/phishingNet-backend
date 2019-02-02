30784f9079f3885271e86ba1dbf37db644044f43
unknown <raj.abhisingh17@gmail.com>
2019-02-03 05:24:32 +0530

    push 3

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 var authenticationRouter = require('./routes/authenticate.js');
 var reportRouter = require('./routes/report.js');
 var searchRouter = require('./routes/search.js');
+var helmet = require('helmet');
 
 var app = express();
 
@@ -12,7 +13,11 @@ app.use('/api/auth',authenticationRouter);
 app.use('/api/report',reportRouter);
 app.use('/api/search',searchRouter);
 
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
+    console.log('Header Added')
     // Website you wish to allow to connect
      res.setHeader('Access-Control-Allow-Origin', '*');
      // Request methods you wish to allow
@@ -24,6 +29,6 @@ app.use((req, res, next) => {
      next();
   });
 
-app.listen(process.env.PORT||3000,(status)=>{
-    console.log('Server up on the port 3000');
+app.listen(process.env.PORT||8080,(status)=>{
+    console.log('Server up on the port 8080');
 });
